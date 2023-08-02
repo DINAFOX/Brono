7ec6555d58d6adde0116f4bd8dd1f804794ed79c
Мадина <madina@MacBook-Air-Madina-2.local>
2023-08-02 16:02:27 +0300

    init

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".banner-next",
+      prevEl: ".banner-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
